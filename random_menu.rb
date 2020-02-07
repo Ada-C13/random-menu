897b85774187660424459a5b87f5704cecfe0f52
Angela <angethuy@gmail.com>
2020-02-06 17:34:34 -0800

    Update random_menu.rb

--- random_menu.rb
+++ random_menu.rb
@@ -2,79 +2,94 @@
 # Program to generate a menu of food items 
 
 # Method to prompt user for list of words used during food generation.
-def populate_array(list)
-  puts "How many #{list}?"
-  size = gets.strip.to_i
-  puts "Great, give me #{size} #{list}."
-  list_of_things = []
-  size.times do |i|
-    print "#{i+1}) is... > "
-    list_of_things << gets.strip.to_s
+
+
+food_components = {
+    adjectives: ["gooey","glowing","melting","suspicious","sticky"],
+    preparations: ["poached","boiled","sauteed","sliced","steamed"],
+    foodstuffs: ["cookies","cake","peas","eggplant","unicorn"]
+}
+
+# Method adds more word options. 
+# Does not accept repeated items.
+def populate_array(list) 
+  list.each do |category, items|
+    puts "How many #{category} do you want to add?"
+    amount_to_add = gets.strip.to_i
+    amount_to_add.times do 
+      print "Add to #{category} > "
+      new_item = gets.strip.to_s
+      while items.include? new_item
+        print "You're trying to add a repeat. Give me a new #{category}. > "
+        new_item = gets.strip.to_s
+      end
+      items << new_item
+
+    end
   end
-  return list_of_things
+  return list
 end
 
-
-def generate_foods(unique, menu_size, food_components)
-  puts "**MENU**\n"
-  if unique #TO-DO : finish writing the unique generation function
-    used = []
-    component_index = i.index(i.sample)
-    while used.include? component_index
-      component_index = i.index(i.sample)
+# Method prompts for user input until a valid y/n response is given.
+# Returns the response in its boolean form.
+def booleanize(input)  
+  until input == true || input == false 
+    case input 
+      when "yes", "y"
+        input = true 
+      when "no", "n"
+        input = false
+      else
+        print "Not a valid y/n response, try again. > "
+        input = gets.strip.to_s.downcase
+      end
     end
-    used << component_index
-    new_food << i[component_index]
-    puts "UNIQUE!"
+  return input
+end
+
+# Method generates new food item.
+def generate_foods(unique, count, food_components)
+  if unique #TO-DO : finish writing the unique generation function
+    puts "UNIQUE MENU PLACEHOLDER!"
   else
-    menu_size.times do |i|
+    count.times do |i|
       print "#{i+1}) "
       new_food = ""
-        food_components.each do |i|
-          new_food << "#{i.sample} "
+        food_components.each do |category, items|
+          new_food << "#{values.sample} "
         end 
         puts "#{new_food}"
       end
-
   end
 end
 
-used = []
-component_index = i.index(i.sample)
 
-while used.include? component_index
-  component_index = i.index(i.sample)
+#Main program loop.
+puts "HELLO, CHEF.\n\n"
+
+print "Feed more words to the generator? y/n > "
+feed_more = gets.strip.to_s.downcase
+
+if booleanize(feed_more)
+  food_components = populate_array(food_components) 
 end
 
-used << component_index
-new food << i[component_index]
+print "How many menu items do you want to generate? > "
+menu_count = gets.strip.to_i
 
-puts "HELLO, CHEF."
-puts "Let's figure out your menu using adjectives, preparations, and foodstuffs."
-puts "First, tell me what we're working with."
-puts "***"
+print "Should they be unique? y/n > "
+unique = booleanize(gets.strip.to_s.downcase)
 
-food_components = []
-food_components << populate_array("adjectives")
-food_components << populate_array("preparations")
-food_components << populate_array("foodstuffs")
-
-puts "How many menu items do you want to generate?"
-menu_size = gets.strip.to_i
-
-puts "Should menu items be unique? (y/n) > "
-ensure_unique = gets.strip.upcase
-
-
-#main loop to generate foods 
-if (ensure_unique == "Y") 
-  if food_components.all? { |s| s.size >= menu_size } #check to make sure there are enough words to generate requested uniques
-    generate_foods(true, menu_size, food_components)
-
+if unique 
+  if food_components.all? { |category, items| items.length >= menu_count } #check to make sure there are enough words to generate requested uniques
+    generate_foods(true, menu_count, food_components)
   else 
     puts "Sorry, you don't have enough words to generate that many unique items."
   end
-else 
-  generate_foods(false, menu_size, food_components)
+else
+  generate_foods(false, menu_count, food_components)
 end
 
+
+
+    
